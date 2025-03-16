313aa53c02ebe9e46fa9a6296310ef603abc84fd
Hemanth Kumar <java.swing.developer@gmail.com>
2025-03-16 13:04:20 +0530

    Functions

--- Functions/app.js
+++ Functions/app.js
@@ -117,4 +117,73 @@ function max(/** ... */) {
 }
 
 var largest = max(1, 10, 100, 2, 3, 1000, 4, 5, 10000, 6);
-console.log(largest);
+//console.log(largest);
+
+/**
+ * Function that uses object properties as arguments
+ */
+// Copy length elements of an array from to the array to.
+// begin copying with element from_start in the from array
+// and copy that element to to_start in the to array
+// it is hard to remember the order of arguments
+function arrayCopy(/* array */ from, /* index */ from_start,
+                    /* array */ to, /* index */ to_start,
+                    /* integer */ length
+) {
+    for (var i = from_start; i < from.length; i++) {
+        to[to_start++] = from[i];
+    }
+}
+
+// This version is a little inefficient, but you don't have to remember the order of the arguments, and from_start and to_start default to 0
+function easyCopy(args) {
+    arrayCopy(args.from, args.from_start || 0, args.to, args.to_start || 2, args.length);
+}
+
+var a =[1, 2, 3, 4];
+var b = [];
+easyCopy({from:a, to:b, length:4});
+//console.log(b);
+
+/**
+ * Functions used as data
+ */
+// To demonstrate using functions as data, first we define few functions
+function add(x, y) {return x+y;}
+function subtraxt(x, y) { return x-y;}
+function multiply(x,y) {return x*y;}
+function divide(x,y) { return x/y;}
+
+// Here's a function that takes one of the above functions as an argument and invokes it on two operands
+function operate(operator, operand1, operand2) {
+    return operator(operand1, operand2);
+}
+
+// We could invoke this function like this to compute the value of (2+3) + (4*5)
+var i = operate(add, (operate(add, 2, 3)), (operate(multiply, 4, 5)));
+
+// For the sake of the example, we implement the simple functions again, this time using function literals within an object literal
+var operators = {
+    add: function (x,y) {return x+y;},
+    subtract: function (x,y) {return x-y;},
+    multiply: function (x,y) { return x*y;},
+    divide: function (x,y) { return x/y;},
+    pow: Math.pow // Works for the predefined functions also
+};
+
+// This function takes the name of the operator, looks up that operator in the object, and then invokes it on the supplied operands.
+// Note the syntax used to invoke the operator function.
+function operate2(operation, operand1, operand2) {
+    if (typeof operators[operation] === "function") {
+        return operators[operation](operand1, operand2);
+    }
+    else {
+        throw 'Unknown Operator';
+    }
+}
+
+// Compute the value of ("hello" + "" + "world") like this
+var j = operate2("add", "hello", operate2("add", " ", "world"));
+var k = operate2("pow", 10, 2);
+//console.log(j + "\n" + k);
+
