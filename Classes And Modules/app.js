76a4968ef842bec1af2e291b7bdf0185b545e38e
Hemanth Kumar <java.swing.developer@gmail.com>
2025-03-20 22:59:56 +0530

    Classes and Modules

--- Classes And Modules/app.js
+++ Classes And Modules/app.js
@@ -11,3 +11,91 @@ function inherit(p) {
     f.prototype = p;
     return new f();
 }
+
+// A simple function for defining simple classes
+function defineClass(constructor, methods, statics) {
+    if (methods) extend(constructor.prototype, methods);
+    if (statics) extend(constructor, statics);
+
+    return constructor;
+}
+
+const student = {
+    fullName: "Hemanth Kumar",
+    marks: 99.9,
+    printMarks: function() {
+        console.log("marks = ", this.marks);
+    }
+};
+
+let arr = ["apple", "mango", "banana"];
+
+
+const employee = {
+    calcTax1() {
+        console.log("tax rate is 10%");
+    },
+    calcTax2: function() {
+        console.log("tax rate is 20%");
+    }
+};
+
+const karanArjun = {
+    salary: 50000
+};
+
+
+karanArjun.__proto__ = employee;
+
+//karanArjun.calcTax2();
+
+class ToyotaCar {
+    constructor(brand) {
+        this.brand = brand;
+        console.log("Creating new " + this.brand);
+    }
+    start() {
+        console.log("Starting...");
+    }
+    
+    stop() {
+        console.log("Stopping...");
+    }
+
+}
+
+//let fortuner = new ToyotaCar("Fortuner");
+//let lexus = new ToyotaCar("Lexus");
+
+class Parent {
+    hello() {
+        console.log("Hello");
+    }
+}
+
+class Child extends Parent {
+
+}
+
+let obj = new Child();
+
+class Person {
+    eat() {
+        console.log("Eating");
+    }
+
+    sleep() {
+        console.log("Sleeping");
+    }
+};
+
+class Engineer extends Person {
+    work() {
+        console.log("solve problems, build something");
+    }
+}
+
+let hemanth = new Engineer();
+hemanth.eat();
+hemanth.work();
+hemanth.sleep();
